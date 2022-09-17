4836a48b1b4b89652e08f2b45cc8f664b1b6f7d8
jun10000 <jun10000@icloud.com>
2022-09-17 23:31:30 +0900

    1. Add redirect page generator. 2. Add generated pages.

---
+++ generate_pages.py
@@ -0,0 +1,35 @@
+import os
+
+html_template = """<html>
+    <head>
+        <meta http-equiv="refresh" content="{{1}};URL='{{2}}'" />
+    </head>
+    <body>
+        {{1}}秒後に新URLにリダイレクトします。<br>
+        移動しない場合は、<a href="{{2}}">こちら</a>をクリックしてください。
+    </body>
+</html>"""
+
+wait_seconds = 0
+html_template = html_template.replace("{{1}}", str(wait_seconds), 2)
+
+redirect_pairs = [
+    ["./sitemap/index.html", "https://jun10000.github.io/"],
+    ["./index.html", "https://jun10000.github.io/"],
+    ["./category/handmade-keyboard/index.html", "https://jun10000.github.io/"],
+    ["./introduce-fortitude60-keymap/index.html", "https://jun10000.github.io/introduce-fortitude60-keymap/"],
+    ["./new-tecsee-switches/index.html", "https://jun10000.github.io/new-tecsee-switches/"],
+    ["./change-key-switch-fortitude60/index.html", "https://jun10000.github.io/change-key-switch-fortitude60/"],
+    ["./lube-keyboard-switch/index.html", "https://jun10000.github.io/lube-keyboard-switch/"],
+    ["./build-log-fortitude60/index.html", "https://jun10000.github.io/build-log-fortitude60/"],
+    ["./privacy-policy/index.html", "https://jun10000.github.io/about/"]
+]
+
+for filepath, new_url in redirect_pairs:
+    html = html_template.replace("{{2}}", new_url, 2)
+    filepath = os.path.abspath(filepath)
+    filedir = os.path.dirname(filepath)
+    os.makedirs(filedir, exist_ok=True)
+
+    with open(filepath, "w", encoding="utf-8") as file:
+        file.write(html)
